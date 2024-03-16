2d488f426e597a0c136307bbe61d8ed00d0ddc58
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-15 23:36:30 -0400

    added background image

---
+++ app/Components/Header.tsx
@@ -0,0 +1,14 @@
+import { BiMenu } from "react-icons/bi";
+
+function Header() {
+  return (
+    <header>
+      <div className="z-1">
+        GRANT
+        <BiMenu className="" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
